9dbd8f472d15224f3f87162ab08c7dd89e1db5cd
Sevdenur <nur.bayram@bil.omu.edu.tr>
2016-07-13 11:27:10 +0300

    yardımcı gemler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UzemTodo::Application.config.secret_key_base = 'eff2701d6660f565a7abe08829521ac87854306aa815f97b9c4d96bfee0f302515ef563ea7aafd5b9f256ff0ca283cad75f69cedb617e93c8a330ba8f0cefb9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UzemTodo::Application.config.session_store :cookie_store, key: '_uzem_todo_session'
